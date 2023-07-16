833f5b34298481befd239d080442aef8a3d7730d
Shubh <shubhjhawar78@gmail.com>
2023-07-16 15:12:31 -0400

    dep err try 1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    // Allow Next.js to use experimental features
+    experimental: {
+      externalDir: true, // Enable external directory support
+    },
+    
+    // Configure Webpack resolution for packages
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        // Ignore 'fs' and 'child_process' packages that are not meant for browser-side execution
+        config.resolve.fallback = {
+          fs: false,
+          child_process: false,
+        }
+      }
+      
+      return config
+    },
+    
+    // Exclude unnecessary dependencies from being bundled
+    excludeDefaultMomentLocales: true,
+    
+    // Enable serverless deployment
+    target: 'serverless',
+    
+    // Add custom environment variables
+    env: {
+      // Add your environment variables here
+    },
+  }
+  
+  module.exports = nextConfig
+  
